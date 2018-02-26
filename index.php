28774a237ceb9c89b12030b21da031ce6857a7e7
eduardomav <eduardomav@gmail.com>
2018-02-26 02:02:46 -0600

    Agregando estructura html5

--- index.php
+++ index.php
@@ -1,1 +1,22 @@
-<?php echo "Hola Mundo"; ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Primer ejemplo con GIT</title>
+	<style type="text/css">
+		body{
+			background: #eee;
+			margin: 0px;
+		}
+		img{
+			max-width: 100%;
+		}
+		
+	</style>
+</head>
+<body>
+	<p>Esta es una página de prueba:</p>
+	<?php echo "<p>Hola Mundo</p>"; ?>
+	<img src="estructura.png">
+
+</body>
+</html>
